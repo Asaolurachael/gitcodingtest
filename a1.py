587385c6eb08f2c79e8775df616600d69fa3d9ff
Asaolurachael <ariom6696@gmail.com>
2024-11-09 16:16:18 +0100

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,35 @@
+friend = ["Bella", "Peace", "Shalom", "Goodness", "Gift"]
+bestfriend = ["Joy", "Goodness"]
+
+for name in friend + bestfriend:
+    if name == 'Bella':
+        print("Bella is my friend")
+    elif not friend:
+        print("Bella is my bestie")
+
+
+    if name == 'Peace':
+        print("Peace is my friend")
+    elif not friend:
+        print("Peace is my bestie")
+
+
+    if name == 'Shalom':
+        print("Shalom is my friend")
+    elif not friend:
+        print("Shalom is my bestie")
+
+    if name == 'Gift':
+        print("Gift is my friend")
+    elif not friend:
+        print("She is my Bestie")
+
+    if name  == 'Goodness':
+        print("Goodness is my Friend and also my bestie")
+    elif not friend:
+        print("Goodness is my friend")
+
+    if name == 'Joy':
+        print("Joy is my bestie")
+    elif not friend:
+        print("Joy is my bestie")
